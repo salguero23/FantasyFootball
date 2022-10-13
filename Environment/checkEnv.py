3b8075d41e5de6202247a6d90a9503f1ec8ad3e3
Andrew Salguero <72628456+salguero23@users.noreply.github.com>
2022-10-13 10:43:36 -0700

    Delete Environment directory

--- Environment/checkEnv.py
+++
@@ -1,8 +0,0 @@
-from stable_baselines3.common.env_checker import check_env
-from Environment.environments import Projecitions
-from datapreper import get_data
-
-trainX, trainy, _, _, _, _ = get_data()
-
-env = Projecitions(X=trainX,y=trainy)
-check_env(env)
